1ec9b4d83b131911871fc01a25f4aba24379bf19
Mustafa GÜVENÇ <mustafa@mustafaguvenc.com>
2019-07-24 20:11:38 +0300

    fix for #9

--- GoDaddy_Bash_DDNS.sh
+++ GoDaddy_Bash_DDNS.sh
@@ -24,22 +24,26 @@ headers="Authorization: sso-key $key:$secret"
 result=$(curl -s -X GET -H "$headers" \
  "https://api.godaddy.com/v1/domains/$domain/records/A/$name")
 
+#echo $result;
+
 dnsIp=$(echo $result | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
-# echo "dnsIp:" $dnsIp
+#echo "dnsIp:" $dnsIp
 
 # Get public ip address there are several websites that can do this.
 ret=$(curl -s GET "http://ipinfo.io/json")
 currentIp=$(echo $ret | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
-# echo "currentIp:" $currentIp
-if [ "$dnsIpa" = "$currentIp" ];
-# if [[ "$dnsIp" =! "$currentIp" ]];
+
+
+#echo "currentIp:" $currentIp
+
+if [ "$dnsIp" != "$currentIp" ];
  then
-#       echo "Ips are not equal"
-        request='[{"data":"'$currentIp'","ttl":3600}]'
-#       echo $request
-        nresult=$(curl -i -s -X PUT \
+#	echo "Ips are not equal"
+	request='[{"data":"'$currentIp'","ttl":3600}]'
+#	echo " request:" $request
+	nresult=$(curl -i -s -X PUT \
  -H "$headers" \
  -H "Content-Type: application/json" \
  -d $request "https://api.godaddy.com/v1/domains/$domain/records/A/$name")
-#       echo $nresult
+#	echo "result:" $nresult
 fi
